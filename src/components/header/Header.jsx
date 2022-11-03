705044efb1fbb1427eef6403e44ee8cb7947e973
Fahmi Ihza <fahmiihza.sa@gmail.com>
2022-11-04 06:15:04 +0700

    update the README.md with link to contributing guide

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import { faBed } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className='headerList'>
+            <div className='headerListItem'>
+            <FontAwesomeIcon icon={faBed} />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/pages/hotel/Hotel.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hotel = () => {
+  return (
+    <div>Hotels</div>
+  )
+}
+
+export default Hotel
