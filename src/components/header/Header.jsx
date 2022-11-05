d0553617c4432534395a4390af2731676477b6a3
Fahmi Ihza <fahmiihza.sa@gmail.com>
2022-11-06 04:56:23 +0700

    commit pagi hari

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,37 @@
-import { faBed } from '@fortawesome/free-solid-svg-icons';
+import { faBed, faCar, faPlane, faTaxi } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './header.css'
 
 const Header = () => {
   return (
     <div className='header'>
-        <div className='headerList'>
+      <div className='headerContainer'>
+      <div className='headerList'>
+            <div className='headerListItem active'>
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+            </div>
+            <div className='headerListItem'>
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flight</span>
+            </div>
+            <div className='headerListItem'>
+            <FontAwesomeIcon icon={faCar} />
+            <span>Car rentals</span>
+            </div>
             <div className='headerListItem'>
             <FontAwesomeIcon icon={faBed} />
+            <span>Attraction</span>
+            </div>
+            <div className='headerListItem'>
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airport taxis</span>
             </div>
         </div>
+        <h1 className='headerTitle'>A lifetime discount? It's Genius.</h1>
+        <p className='headerDesc'>Get rewarded for your travels - unlock instant savings of 10% or more with a free Fambooking account
+        </p>   
+      </div>
     </div>
   )
 }
